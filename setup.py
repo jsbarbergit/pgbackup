f6cb82cdc4a166ccb0ba9da3ec357237593e6aa6
John Barber <jsbarber@gmail.com>
2018-03-14 08:53:21 +0000

    Project structure created with Makefile and setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+# Load existing README.rst for long description
+with open('README.rst', encoding='UTF-8') as f:
+    readme = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    description='Database backups locallly or to AWS S3',
+    long_description=readme,
+    author='John Barber',
+    author_email='jsbarber@gmail.com',
+    install_requires=[],
+    # Will recursively look through given dir for __init__.py to indicate req'd pkgs
+    packages=find_packages('src'),
+    package_dir={'':'src'}
+)
